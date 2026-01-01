78e65f50b39d2bb61cbe4b6348933fda61b70fe2
ISTKHAR-CODER786 <istkharalam6202@gmail.com>
2026-01-01 14:50:32 +0530

    Update mongo.py

--- ZOYA_MUSIC/core/mongo.py
+++ ZOYA_MUSIC/core/mongo.py
@@ -1,14 +1,23 @@
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
+LOGGER(__name__).info("» ᴄᴏɴɴᴇᴄᴛɪɴɢ ᴛᴏ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Yukki
-    LOGGER(__name__).info("Connected to your Mongo Database.")
+    LOGGER(__name__).info("» ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ.")
 except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    LOGGER(__name__).error("» ꜰᴀɪʟᴇᴅ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ.")
     exit()
+
+# ===========================================================
+# ©️ 2025-26 All Rights Reserved by Team Rocky (Im-Notcoder) 😎
+# 
+# 🧑‍💻 Developer : t.me/Mrrockytg
+# 🔗 Source link : t.me/rockyxsupport
+# 📢 Telegram channel : t.me/rockyxUpdate
+# ===========================================================
